b6af622d04e1c0cf7666eb87a633ce549fb584e1
Tobia Ochsner <tobia.ochsner@hotmail.com>
2024-06-27 14:53:46 +0200

    Init repo.

---
+++ beast_benchmarks/run_benchmarks.sh
@@ -0,0 +1,19 @@
+sbatch --cpus-per-task=8 --mem-per-cpu=2G --output out/bdmmflow_1.out --wrap="../beast/bin/beast -overwrite H3N2_analysis_dataset_500seq_bdmmflow.xml"
+sbatch --cpus-per-task=8 --mem-per-cpu=2G --output out/bdmmflow_inverse_1.out --wrap="../beast/bin/beast -overwrite H3N2_analysis_dataset_500seq_bdmmflow_inverse.xml"
+sbatch --cpus-per-task=8 --mem-per-cpu=2G --output out/bdmmprime_1.out --wrap="../beast/bin/beast -overwrite H3N2_analysis_dataset_500seq_bdmmprime_parallelized.xml" --time=20:00:00
+
+sbatch --cpus-per-task=8 --mem-per-cpu=2G --output out/bdmmflow_2.out --wrap="../beast/bin/beast -overwrite H3N2_analysis_dataset_500seq_bdmmflow.xml"
+sbatch --cpus-per-task=8 --mem-per-cpu=2G --output out/bdmmflow_inverse_2.out --wrap="../beast/bin/beast -overwrite H3N2_analysis_dataset_500seq_bdmmflow_inverse.xml"
+sbatch --cpus-per-task=8 --mem-per-cpu=2G --output out/bdmmprime_2.out --wrap="../beast/bin/beast -overwrite H3N2_analysis_dataset_500seq_bdmmprime_parallelized.xml" --time=20:00:00
+
+sbatch --cpus-per-task=8 --mem-per-cpu=2G --output out/bdmmflow_3.out --wrap="../beast/bin/beast -overwrite H3N2_analysis_dataset_500seq_bdmmflow.xml"
+sbatch --cpus-per-task=8 --mem-per-cpu=2G --output out/bdmmflow_inverse_3.out --wrap="../beast/bin/beast -overwrite H3N2_analysis_dataset_500seq_bdmmflow_inverse.xml"
+sbatch --cpus-per-task=8 --mem-per-cpu=2G --output out/bdmmprime_3.out --wrap="../beast/bin/beast -overwrite H3N2_analysis_dataset_500seq_bdmmprime_parallelized.xml" --time=20:00:00
+
+sbatch --cpus-per-task=8 --mem-per-cpu=2G --output out/bdmmflow_4.out --wrap="../beast/bin/beast -overwrite H3N2_analysis_dataset_500seq_bdmmflow.xml"
+sbatch --cpus-per-task=8 --mem-per-cpu=2G --output out/bdmmflow_inverse_4.out --wrap="../beast/bin/beast -overwrite H3N2_analysis_dataset_500seq_bdmmflow_inverse.xml"
+sbatch --cpus-per-task=8 --mem-per-cpu=2G --output out/bdmmprime_4.out --wrap="../beast/bin/beast -overwrite H3N2_analysis_dataset_500seq_bdmmprime_parallelized.xml" --time=20:00:00
+
+sbatch --cpus-per-task=8 --mem-per-cpu=2G --output out/bdmmflow_5.out --wrap="../beast/bin/beast -overwrite H3N2_analysis_dataset_500seq_bdmmflow.xml"
+sbatch --cpus-per-task=8 --mem-per-cpu=2G --output out/bdmmflow_inverse_5.out --wrap="../beast/bin/beast -overwrite H3N2_analysis_dataset_500seq_bdmmflow_inverse.xml"
+sbatch --cpus-per-task=8 --mem-per-cpu=2G --output out/bdmmprime_5.out --wrap="../beast/bin/beast -overwrite H3N2_analysis_dataset_500seq_bdmmprime_parallelized.xml" --time=20:00:00
